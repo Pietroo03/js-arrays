861802034d1cd3c951ab84631d1977fe9092d219
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-02 16:04:34 +0200

    complete exercise 3 folder 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,7 +11,7 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = teachers.reverse();
-/* console.log(reversedTeachers); */
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -24,3 +24,10 @@ for (let i = 0; i < teachers.length; i++){
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+let index = teachers.indexOf('Ed')
+
+if (index !== -1) {
+  teachers.splice(index,1)
+}
+
+console.log(teachers);
